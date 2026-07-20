26165126213eb7b2330db407ca444811068e4598
AryanDash77 <aryandash946@gmail.com>
2026-07-20 21:53:41 +0530

    Actually completed now Task 8

--- window_funcs.sql
+++ window_funcs.sql
@@ -11,7 +11,7 @@ from transactions;
 -- Display full name, credit score, and both rank columns.
 
 select concat(first_name , ' ' , last_name) as full_name , 
-credit_score , rank() over (order by credit_score) as rnk ,
+credit_score , rank() over (order by credit_score desc) as rnk ,
 dense_rank() over (order by credit_score) as drnk
 from customers;
 
@@ -26,16 +26,42 @@ order by credit_score desc) as rnk from customers;
 -- For each account, show each transaction amount alongside the previous transaction amount using LAG. 
 -- Display account_id, transaction_date, amount, and prev_amount. 
 
-
+select account_id , transaction_date , amount ,
+lag(amount) over (partition by account_id order by transaction_date)
+as prev_amount from transactions;
 
 -- Q5) 
 -- For each account, show each transaction amount alongside the next transaction amount using LEAD. 
+
+select account_id , transaction_date , amount ,
+lead(amount) over (partition by account_id order by transaction_date)
+as next_amount from transactions;
+
 -- Q6) 
 -- Calculate a running total of transaction amounts per account ordered by date. 
 -- Display account_id, transaction_date, amount, and running_total.
+
+select account_id , transaction_date , amount , 
+sum(amount) over (partition by account_id order by transaction_date)
+as running_total from transactions;
+
 -- Q7) 
 -- Calculate a 3-transaction moving average of amounts per account. 
--- Display account_id, transaction_date, amount, and moving_avg. (Hint: use ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+-- Display account_id, transaction_date, amount, and moving_avg. 
+
+select account_id , amount , transaction_date , 
+avg(amount) over (partition by account_id order by
+transaction_date rows between 2 preceding and current row)
+as moving_avg from transactions;
+
 -- Q8) 
--- For each city, show each customer alongside the highest credit score in their city and the lowest credit score in their city. 
+-- For each city, show each customer alongside the highest credit score
+-- in their city and the lowest credit score in their city. 
 -- Use FIRST_VALUE and LAST_VALUE. (Hint: ORDER BY credit score DESC for FIRST_VALUE to give you the highest)
+
+select concat(first_name , ' ' , last_name) as full_name ,
+city , credit_score , first_value(credit_score) over 
+(partition by city order by credit_score desc) as hsal ,
+last_value(credit_score) over (partition by city
+order by credit_score desc rows between unbounded preceding
+and unbounded following) as lsal from customers;
